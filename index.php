61e47c8cdf9c6ce30da811323ccd292b5784d95d
LukaKoporec <Luka.koporec123@gmail.com>
2021-03-09 18:36:19 +0100

    Learned how to use loops

--- index.php
+++ index.php
@@ -1,28 +1,61 @@
 <?php
-//$vartest
-     //$myVar1 = "Hello";
-     //$myVar2 = "World";
+/*
+$vartest
+     $myVar1 = "Hello";
+     $myVar2 = "World";
 
-     //echo $myVar1."   ".$myVar2;
+     echo $myVar1."   ".$myVar2;
 
-     //$arrayName = array('' => , );
-     //$numbers = array(12,45,22,34,65);
-     //$numbers = [12,45,22,34,65];
+$arrayName = array('' => , );
+$numbers = array(12,45,22,34,65);
+$numbers = [12,45,22,34,65];
 
-//print_r($numbers);
-//echo $numbers[1];
+print_r($numbers);
+echo $numbers[1];
 
+     $ages = array(
+    "John" => 35,
+    "Mary" => 27,
+    "Bob" => 55
+     );
+
+    echo $ages["Mary"];
+
+    array_pop($ages); //get rid of Bob
+    array_shift($ages); //get rid of John
+
+    print_r($ages);
+
+
+
+LOOP (for)
+    for ($i=0; $i < 5; $i++) {
+      echo $i;
+    }
+
+
+
+LOOP (while)
+
+$i= 0;
+while ($i <= 10) {
+  echo "Number ".$i."<br  />";
+  $i++;
+
+
+
+LOOP (foreach)
+}
+*/
 $ages = array(
 "John" => 35,
 "Mary" => 27,
 "Bob" => 55
 );
 
-//echo $ages["Mary"];
+foreach ($ages as $name => $age) {
+  echo $name." is ". $age. " years old<br  />";
+}
 
-array_pop($ages); //get rid of Bob
-array_shift($ages); //get rid of John
-
-print_r($ages);
 
 ?>
