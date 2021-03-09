9509e6119073ede56b17bb876d3f986bf46899fa
LukaKoporec <Luka.koporec123@gmail.com>
2021-03-09 18:15:56 +0100

    tried array_pop and shift

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
 <?php
+//$vartest
      //$myVar1 = "Hello";
      //$myVar2 = "World";
 
      //echo $myVar1."   ".$myVar2;
-$numbers = array(12,45,22,34,65);
 
-print_r($numbers);     
+     //$arrayName = array('' => , );
+     //$numbers = array(12,45,22,34,65);
+     //$numbers = [12,45,22,34,65];
+
+//print_r($numbers);
+//echo $numbers[1];
+
+$ages = array(
+"John" => 35,
+"Mary" => 27,
+"Bob" => 55
+);
+
+//echo $ages["Mary"];
+
+array_pop($ages); //get rid of Bob
+array_shift($ages); //get rid of John
+
+print_r($ages);
+
 ?>
