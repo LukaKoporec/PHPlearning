a14c9b12f252430c9cd7a364013453554ac210ec
LukaKoporec <Luka.koporec123@gmail.com>
2021-03-10 15:30:15 +0100

    Classes & Methods

--- index.php
+++ index.php
@@ -103,7 +103,33 @@ else {
 
 */
 
+class User{
+  public function __construct(){
+    //echo 'Constructor Called';
+  }
 
+  public function register(){
+  echo 'User Registered';
+}
+
+public function login($username, $password){
+  $this->auth_user($username, $password);
+
+}
+
+public function auth_user($username, $password){
+  echo $username. ' is authenticated';
+}
+
+public function __destruct(){
+  //echo 'Destructor Called';
+}
+}
+
+$User = new User;
+
+//$User->register();
+$User->login('Brad', '1234');
 
 
 
