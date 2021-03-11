cfc261088bba17128b24e39852224085d1a0debb
LukaKoporec <Luka.koporec123@gmail.com>
2021-03-11 15:49:59 +0100

    updated

--- index.php
+++ index.php
@@ -281,27 +281,21 @@ $people->iterateObject();
 
 */
 
+
 require 'classes/Database.php';
 
 $database = new Database;
 
-$database->query('SELECT * FROM posts');
-
+$database->query('SELECT * FROM posts WHERE id = :id');
+$database->bind(':id', 2);
 $rows = $database->resultset();
 ?>
 <h1>Posts</h1>
-<div class="">
-<?php
-foreach($rows as $row) : ?>
 <div>
-  <h3><?php echo $row['Title']; ?></h3>
-  <p><?php echo $row['Body']; ?></p>
-</div>
+<?php foreach($rows as $row) : ?>
+	<div>
+		<h3><?php echo $row['title']; ?></h3>
+		<p><?php echo $row['body']; ?></p>
+	</div>
 <?php endforeach; ?>
 </div>
-
-
-
-
-
-?>
