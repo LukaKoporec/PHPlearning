3b9792feead09250fdcb917b2205c40df6e0e74d
LukaKoporec <Luka.koporec123@gmail.com>
2021-03-11 15:40:52 +0100

    Database

--- index.php
+++ index.php
@@ -281,6 +281,25 @@ $people->iterateObject();
 
 */
 
+require 'classes/Database.php';
+
+$database = new Database;
+
+$database->query('SELECT * FROM posts');
+
+$rows = $database->resultset();
+?>
+<h1>Posts</h1>
+<div class="">
+<?php
+foreach($rows as $row) : ?>
+<div>
+  <h3><?php echo $row['Title']; ?></h3>
+  <p><?php echo $row['Body']; ?></p>
+</div>
+<?php endforeach; ?>
+</div>
+
 
 
 
